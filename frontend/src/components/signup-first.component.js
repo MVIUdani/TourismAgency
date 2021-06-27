487c4ee8aff4ccf40a05c266a80e7a56766faec1
MVIUdani <imeudani@gmail.com>
2021-06-28 00:59:57 +0700

    Create login and signup components

---
+++ frontend/src/components/signup-first.component.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+        <form>
+  <div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action">SignUp as a Customer</a>
+  <a href="#" class="list-group-item list-group-item-action">SignUp as a Guide</a>
+  <a href="#" class="list-group-item list-group-item-action">SignUp as a Hotel Owner</a>
+  <a href="#" class="list-group-item list-group-item-action">SignUp as a Transport Owner</a>
+  </div>
+       </form>
+        );
+    }
+}
