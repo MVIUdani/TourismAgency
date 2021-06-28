52d07ac0fd339de76254079da203cd01c0ce69c0
MVIUdani <imeudani@gmail.com>
2021-06-28 18:10:32 +0700

    ajust folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,26 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Login from "./components/login.component";
-import SignUp from "./components/signup-first.component";
-import Home from "./components/home.component";
+import Login from "./components/js/login.component";
+import SignUp from "./components/js/signup-first.component";
+import Home from "./components/js/home.component";
+import Aboutus from "./components/js/aboutus.component";
 
 function App() {
   return (<Router>
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div className="container">
-          <Link className="navbar-brand" to={"/home"}>Paradise Lanka Tours</Link>
+          <Link className="navbar-brand" to={"/"}>Paradise Lanka Tours</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <Link className="nav-link" to={"/home"}>Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/aboutus"}>About Us</Link>
+              </li>
+            </ul> 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-in"}>Login</Link>
@@ -30,9 +39,11 @@ function App() {
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Switch>
+             <Route exact path='/' component={Home} />
             <Route exact path='/home' component={Home} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
+            <Route path="/aboutus" component={Aboutus} />
           </Switch>
         </div>
       </div>
---
+++ frontend/src/components/js/aboutus.component.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+export default class Aboutus extends Component {
+   
+}
