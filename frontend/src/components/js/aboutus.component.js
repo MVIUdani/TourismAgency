8e86e2a765906150dfece9e8b16c566cdf631bb3
Imesha9725 <imesharishani9725@gmail.com>
2021-07-03 09:25:18 +0530

    About Us and Signup

--- frontend/src/components/js/aboutus.component.js
+++ frontend/src/components/js/aboutus.component.js
@@ -1,5 +1,35 @@
 import React, { Component } from "react";
+import '../css/aboutus.css';
 
 export default class Aboutus extends Component {
-   
+    render() {
+        return (
+    <div className="A">
+
+    <br></br><center><h1><i>PARADISE LANKA TOURS</i></h1></center>
+
+    <center><h2>About Us</h2></center><br></br>
+    <p> We are a agencey that determined to uplif the freelancing community in Sri Lanka.We provide many useful services through our agencey.</p>
+    <br></br>
+
+    <center>Our Services for,</center>
+    <br></br>
+      <div className="B">
+          
+              <i><h4>Customer</h4></i>
+              <i><h4>Hotel</h4></i>
+              <i><h4>Guides</h4></i>
+              <i><h4>Transport Owners</h4></i>
+
+              
+      </div>
+<br></br><br></br><br></br>
+  <center><b>You can join and get the services from our agencey!</b></center>
+  
+
+</div>
+
+);
+}
+
 }
