6481a51a419c225b99128167300564e8002e2bfc
MVIUdani <imeudani@gmail.com>
2021-07-05 01:06:00 +0700

    create feedback and contact us pages

---
+++ frontend/src/components/js/home/footer static pages/feedback.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "../../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import '../../../css/footer static pages/feedback.css';
+
+export default class Feedback extends Component{
+   render(){
+       return(
+        <div className="feedback-wrapper">
+        <div className="feedback-inner">
+          <form>
+            <h2>Give Your Feedback</h2> 
+            <div className="form-group">
+                    <label>Your Feedback</label>
+                    <input type="text" className="form-control" placeholder="Summary" />
+                </div>
+                <br/>
+                <div className="form-group">
+                    <label>Futher Details</label>
+                    <textarea className="form-control" placeholder="Optionally add more details here..." />
+                </div>
+                <br/>
+            <button type="submit" className="btn btn-primary btn-block">Submit</button>
+
+          </form>
+          </div>
+          </div>
+       );
+   } 
+}
