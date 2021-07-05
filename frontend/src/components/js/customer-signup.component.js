555d02dcfd27caf4b6146303e3920b1b28897e71
MVIUdani <imeudani@gmail.com>
2021-07-06 00:45:46 +0700

    validate customer signup form

--- frontend/src/components/js/customer-signup.component.js
+++ frontend/src/components/js/customer-signup.component.js
@@ -1,32 +1,92 @@
 import React, { Component } from "react";
 import '../css/authentication.css';
+import validate from './validateInfo';
+import useForm from "./useForm";
 
-export default class CustomerSignUp extends Component {
-    render() {
+const CustomerSignup = ({ submitForm }) => {
+    const { handleChange, handleSubmit, values, errors } = useForm(
+      submitForm,
+      validate
+    ); 
         return (
         <div className="auth-wrapper">
         <div className="auth-inner">
-            <form>
+            <form onSubmit={handleSubmit} noValidate>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input 
+                    type="text" 
+                    className="form-control" 
+                    name="firstname"
+                    placeholder="Enter your first name" 
+                    value={values.firstname}
+                    onChange={handleChange}
+                    />
+                    {errors.firstname && <p>{errors.firstname}</p>}
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" 
+                    className="form-control"
+                    name="lastname" 
+                    placeholder="Enter your last name" 
+                    value={values.lastname}
+                    onChange={handleChange}
+                    />
+                    {errors.lastname && <p>{errors.lastname}</p>}
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" 
+                    className="form-control"
+                    name="email" 
+                    placeholder="Enter your email address"
+                    value={values.email}
+                    onChange={handleChange} 
+                    />
+                    {errors.email && <p>{errors.email}</p>}
+                </div>
+
+                <div className="form-group">
+                    <label>User name</label>
+                    <input type="text" 
+                    className="form-control"
+                    name="username" 
+                    placeholder="Enter your user name" 
+                    value={values.username}
+                    onChange={handleChange}
+                    />
+                    {errors.username && <p>{errors.username}</p>}
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input 
+                    type="password" 
+                    className="form-control"
+                    name="password" 
+                    placeholder="Enter your password"
+                    value={values.password}
+                    onChange={handleChange} 
+                    />
+                    {errors.password && <p>{errors.password}</p>}
+                </div>
+
+                <div className="form-group">
+                    <label>Confirm Password</label>
+                    <input 
+                    type="confirmpassword" 
+                    className="form-control"
+                    name="confirmpassword" 
+                    placeholder="Confirm your password"
+                    value={values.confirmpassword}
+                    onChange={handleChange} 
+                    />
+                    {errors.confirmpassword && <p>{errors.confirmpassword}</p>}
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
@@ -37,5 +97,7 @@ export default class CustomerSignUp extends Component {
             </div>
             </div>
         );
-    }
+    
 }
+
+export default CustomerSignup;
