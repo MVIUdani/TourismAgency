b220074e3a0f1e4a4fda2d95d1153a146c584bb5
MVIUdani <imeudani@gmail.com>
2021-07-06 11:09:07 +0700

    validate form

--- frontend/src/components/js/login.component.js
+++ frontend/src/components/js/login.component.js
@@ -1,22 +1,114 @@
 import React, { Component } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
 import '../css/authentication.css';
 
+
+const required = value => {
+    if (!value) {
+      return (
+        <div>
+          This field is required
+        </div>
+      );
+    }
+  };
+
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+    
+        this.state = {
+          username: "",
+          password: "",
+          loading: false,
+          message: ""
+        };
+      }
+    
+      onChangeUsername(e) {
+        this.setState({
+          username: e.target.value
+        });
+      }
+    
+      onChangePassword(e) {
+        this.setState({
+          password: e.target.value
+        });
+      }
+    
+      handleLogin(e) {
+        e.preventDefault();
+    
+        this.setState({
+          message: "",
+          loading: true
+        });
+    
+        this.form.validateAll();
+    
+       /* if (this.checkBtn.context._errors.length === 0) {
+          AuthService.login(this.state.username, this.state.password).then(
+            () => {
+              this.props.history.push("/");
+              window.location.reload();
+            },
+            error => {
+              const resMessage =
+                (error.response &&
+                  error.response.data &&
+                  error.response.data.message) ||
+                error.message ||
+                error.toString();
+    
+              this.setState({
+                loading: false,
+                message: resMessage
+              });
+            }
+          );
+        } else {
+          this.setState({
+            loading: false
+          });
+        }*/
+      }
+
+
     render() {
         return (
         <div className="auth-wrapper">
         <div className="auth-inner">
-            <form>
+            <Form onSubmit={this.handleLogin}
+            ref={c => {
+              this.form = c;
+            }}>
                 <h3>Login</h3>
 
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>User name</label>
+                    <Input type="username" 
+                    className="form-control" 
+                    placeholder="Enter your user name"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                    validations={[required]} 
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <Input type="password" 
+                    className="form-control" 
+                    placeholder="Enter your password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                    validations={[required]} 
+                    />
                 </div>
 
                 <div className="form-group">
@@ -30,7 +122,7 @@ export default class Login extends Component {
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
-            </form>
+            </Form>
             </div>
             </div>
         );
