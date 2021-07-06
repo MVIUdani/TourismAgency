48c5b9ae106fd710b953b6ad6e0791bdfe5e5284
MVIUdani <imeudani@gmail.com>
2021-07-06 16:55:16 +0700

    create payment checkout pages

---
+++ frontend/src/components/js/customer/payment-first.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import '../../css/customer/payment.css'
+
+export default class PaymentFirst extends Component {
+    render(){
+        return (
+          <div className="app-container">
+            <div className="row">
+              <div className="col">
+                <Item name="Package 1" price="50$" img="logo.jpg" />
+              </div>
+              <div className="col no-gutters">
+              <div className="checkout">
+              <div className="checkout-container">
+                    <h2>Select your Payment Method</h2>
+                    <br></br><br></br>
+                    <li><Link to='/'>Credit or Debit Card</Link></li>
+                    <br></br>
+                    <li><Link to='/'>Payhere</Link></li>
+                </div>
+                </div>
+              </div>
+            </div>
+           </div>
+        );
+      }
+    }
+
+    const Item = (props) => (
+        <div className="item-container">
+          <div className="item-image">
+            <img src={props.img}/>
+            <div className="item-details">
+              <h2 className="item-name"> {props.name} </h2>
+              <h2 className="item-price"> {props.price} </h2>
+            </div>
+          </div>
+        </div>
+      );
+
+    
---
+++ frontend/src/components/js/customer/payment.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import '../../css/customer/payment.css';
+
+export default class Payment extends Component {
+    render(){
+        return (
+          <div className="app-container">
+            <div className="row">
+              <div className="col">
+                <Item name="Package 1" price="50$" img="logo.jpg" />
+              </div>
+              <div className="col no-gutters">
+              <Checkout />
+              </div>
+            </div>
+           </div>
+        );
+      }
+    }
+
+    const Item = (props) => (
+        <div className="item-container">
+          <div className="item-image">
+            <img src={props.img}/>
+            <div className="item-details">
+              <h2 className="item-name"> {props.name} </h2>
+              <h2 className="item-price"> {props.price} </h2>
+            </div>
+          </div>
+        </div>
+      );
+
+
+const Checkout = (props) => (
+ <div className="checkout">
+    <div className="checkout-container">
+     <h3 className="heading-3">Credit or Debit Card Checkout</h3>
+     <Input label="Cardholder's Name" type="text" name="name" />
+     <Input label="Card Number" type="number" name="card_number" imgSrc="
+     " />
+      <div className="row">
+        <div className="col">
+          <Input label="Expiration Date" type="date" name="exp_date" />
+        </div>
+        <div className="col">
+          <Input label="CVV" type="number" name="cvv" />
+        </div>
+      </div>
+      <Button text="Make Payment" />
+    </div>
+ </div>
+);
+
+const Input = (props) => (
+  <div className="input">
+    <label>{props.label}</label>
+    <div className="input-field">
+      <input type={props.type} name={props.name} />
+      <img src={props.imgSrc}/>
+    </div>
+  </div>
+);
+
+const Button = (props) => (
+  <button className="checkout-btn" type="button">{props.text}</button>
+);
