f7bfbc487d8f01c984c6c8198b9a7697a152db44
MVIUdani <imeudani@gmail.com>
2021-07-24 23:44:36 +0700

    mod rating and hide

--- frontend/src/components/js/customer/rating.js
+++ frontend/src/components/js/customer/rating.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
@@ -20,4 +20,13 @@ export default function SimpleRating() {
       </Box>
       </div>
   );
-        }
+        }*/
+
+import React from 'react';
+import { Component } from 'react';
+
+export default class SimpleRating extends Component{
+    render(){
+      return null;
+    }
+    }
