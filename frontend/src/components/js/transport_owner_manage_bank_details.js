f78978223a4a26cbe27c7b7a984079c816a7922d
Imesha9725 <imesharishani9725@gmail.com>
2021-07-25 18:14:54 +0530

    Bank details edit ui create

--- frontend/src/components/js/transport_owner_manage_bank_details.js
+++ frontend/src/components/js/transport_owner_manage_bank_details.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
-
+import  styles from '../css/transport_owner_account.css'
 import { Link } from 'react-router-dom';
 
 function Transport_banak_account(){
 
     return(
 
-        <div>
+              
+<div className="Account_update">
+<form>
+<div className={styles.container}>
+    <center><h1>Bank Account Details</h1></center>
+    <p>Please fill your bank account details.</p>
+    <hr></hr>
 
-            <h2>Imesha Rishani</h2>
-        </div>
+    <label htmlFor=""><h6>Account owner's name</h6></label>
+    <input type="text" placeholder="" name="branch_name" id="" required></input>
+
+    <label htmlFor=""><h6>Bank Name</h6></label>
+    <input type="text" placeholder="" name="bank_name" id="" required></input>
+
+
+    <label htmlFor=""><h6>Branch Name</h6></label>
+    <input type="text" placeholder="" name="branch_name" id="" required></input>
+
+    
+    <label htmlFor=""><h6>Account Number</h6></label>
+    <input type="int" placeholder="" name="branch_name" id="" required></input>
+
+    <input type="hidden" placeholder="" name="user_id" id="" required></input>
+
+
+    <hr></hr>
+
+    <button type="submit" className="registerbtn">Edit</button>
+    
+
+  </div>
+
+</form>
+</div>
+
+
+
+
 
     );
 
