2b0c16323c90250a1e0066947861acd97768857b
MVIUdani <imeudani@gmail.com>
2021-08-01 00:39:51 +0700

    create hotel owner signup backend

--- backend/routes/jwtAuth.js
+++ backend/routes/jwtAuth.js
@@ -8,6 +8,7 @@ const authorize = require("../middleware/authorize");
 
 //authorizeentication
 
+//***this is customer signup***
 router.post("/cus-signup", validInfo, async (req, res) => {
   // const { firstname,lastname,email, username, password } = req.body;
 
@@ -53,10 +54,53 @@ router.post("/cus-signup", validInfo, async (req, res) => {
   }
 });
 
+// ***this is hotel owner signup***
+router.post("/hotelowner-signup", validInfo, async (req, res) => {
+  // const { firstname,lastname,email, username, password, hotel, hotelcontact, address } = req.body;
+
+  try {
+    const { firstname, lastname, email, username, password, hotel, hotelcontact, address } = req.body;
+
+    con.query("SELECT * FROM users WHERE email_address = ?", [email], function (err, result, fields) {
+      if (err)
+        throw err;
+      if (result.length < 0) {
+        return res.status(401).send("User already exist!");
+      }
+    });
+
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+    const bcryptPassword = await bcrypt.hash(password, salt);
 
 
+   /* const newUser = await con.query(
+      "INSERT INTO users (first_name,last_name,email_address, user_password,user_name,hotel, hotelcontact, address ) VALUES (?,?,?, ?, ?) ",
+      [firstname, lastname, email, bcryptPassword, username]
+    );*/
+    let token = null;
+    con.query("INSERT INTO users (first_name,last_name,email_address, user_password,user_name,hotel_name,hotel_contact_no,city,role_id) VALUES (?,?,?, ?, ?,?,?,?,'5') ", 
+    [firstname, lastname, email, bcryptPassword, username, hotel, hotelcontact, address],
+      function (err, result, fields) {
+        if (err)
+          throw err;
+        if (result) {
+          token = jwtGenerator(result.insertId);
+        }
+      });
+
+    if (token) {
+      return res.json({ token });
+    }
 
 
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// ***this is guide signup***
 router.post("/guide-signup", validInfo, async (req, res) => {
   // const { firstname,lastname,email, username, password, nic, birthday } = req.body;
 
@@ -81,7 +125,7 @@ router.post("/guide-signup", validInfo, async (req, res) => {
       [firstname, lastname, email, bcryptPassword, username]
     );*/
     let token = null;
-    con.query("INSERT INTO users (first_name,last_name,email_address, user_password,user_name,nic,birthday,role_id) VALUES (?,?,?, ?, ?,'3') ", 
+    con.query("INSERT INTO users (first_name,last_name,email_address, user_password,user_name,nic,birthday,role_id) VALUES (?,?,?, ?, ?,?,?,'3') ", 
     [firstname, lastname, email, bcryptPassword, username, nic, birthday],
       function (err, result, fields) {
         if (err)
@@ -102,7 +146,7 @@ router.post("/guide-signup", validInfo, async (req, res) => {
   }
 });
 
-
+// ***this is transport owner signup***
 router.post("/transportowner-signup", validInfo, async (req, res) => {
   // const { firstname,lastname,email, username, password, nic, birthday } = req.body;
 
@@ -127,7 +171,7 @@ router.post("/transportowner-signup", validInfo, async (req, res) => {
       [firstname, lastname, email, bcryptPassword, username]
     );*/
     let token = null;
-    con.query("INSERT INTO users (first_name,last_name,email_address, user_password,user_name,nic,birthday,role_id) VALUES (?,?,?, ?, ?,'4') ", 
+    con.query("INSERT INTO users (first_name,last_name,email_address, user_password,user_name,nic,birthday,role_id) VALUES (?,?,?, ?, ?,?,?,'4') ", 
     [firstname, lastname, email, bcryptPassword, username, nic, birthday],
       function (err, result, fields) {
         if (err)
@@ -148,7 +192,7 @@ router.post("/transportowner-signup", validInfo, async (req, res) => {
   }
 });
 
-
+// ***login of all users***
 router.post("/sign-in", validInfo, async (req, res) => {
   //const { email, password } = req.body;
 
--- frontend/src/components/js/hotelowner-signup.component.js
+++ frontend/src/components/js/hotelowner-signup.component.js
@@ -1,12 +1,58 @@
 import React, { Component,useState } from "react";
 import '../css/authentication.css';
 import { Link } from "react-router-dom";
+//import { toast } from "react-toastify";
 
-function HotelOwner_Signup(){
+const HotelOwner_Signup=({setAuth})=>{
+
+    const [inputs, setInputs] = useState({
+        firstname:"",
+        lastname:"",
+        email: "",
+        username: "",
+        password: "",
+        hotel: "",
+        hotelcontact: "",
+        address: "",
+      });
+      const { firstname,lastname,email,username,password,hotel,hotelcontact,address} = inputs;
+
+      const onChange = e =>
+      setInputs({ ...inputs, [e.target.name]: e.target.value });
+  
+    const onSubmitForm = async (e) => {
+      e.preventDefault();
+      try {
+        const body = { firstname,lastname,email,username,password,hotel,hotelcontact,address };
+        const response = await fetch(
+          "http://localhost:5000/auth/hotelowner-signup",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify(body)
+          }
+        );
+        const parseRes = await response.json();
+      
+        if (parseRes.token) {
+          localStorage.setItem("token", parseRes.token);
+          setAuth(true);
+       //  toast.success("Register Successfully");
+        } else {
+          setAuth(false);
+       //  toast.error(parseRes);
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+    
     return(
         <div className="auth-wrapper">
         <div className="auth-inner">
-            <form >
+            <form onSubmit={onSubmitForm} >
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
@@ -16,7 +62,9 @@ function HotelOwner_Signup(){
                     //type="text" 
                     className="form-control" 
                     name="firstname"
-                    placeholder="Enter your first name" 
+                    placeholder="Enter your first name"
+                    value={firstname}
+                    onChange={e => onChange(e)} 
                     required
                     />
                 </div>
@@ -27,7 +75,9 @@ function HotelOwner_Signup(){
                     //type="text"  
                     className="form-control"
                     name="lastname" 
-                    placeholder="Enter your last name" 
+                    placeholder="Enter your last name"
+                    value={lastname}
+                    onChange={e => onChange(e)} 
                     required
                     />
                 </div>
@@ -39,6 +89,8 @@ function HotelOwner_Signup(){
                     className="form-control"
                     name="email" 
                     placeholder="Enter your email address"
+                    value={email}
+                    onChange={e => onChange(e)}
                     required
                     />
                     
@@ -50,7 +102,9 @@ function HotelOwner_Signup(){
                     //type="text"
                     className="form-control"
                     name="username" 
-                    placeholder="Enter your user name" 
+                    placeholder="Enter your user name"
+                    value={username}
+                    onChange={e => onChange(e)} 
                     required
                     />
                 </div>
@@ -62,6 +116,8 @@ function HotelOwner_Signup(){
                     className="form-control"
                     name="password" 
                     placeholder="Enter your password"
+                    value={password}
+                    onChange={e => onChange(e)}
                     required 
                     />
                 </div>
@@ -74,18 +130,36 @@ function HotelOwner_Signup(){
                     className="form-control"
                     name="hotelname"
                     placeholder="Enter your hotel name"
+                    value={hotel}
+                    onChange={e => onChange(e)}
                     required
                     />
                 </div>
 
                 <div className='form-group'>
-                    <label>Address of hotel</label>
+                 <label>Hotel Contact No</label>
+                 <input
+                 type="bigint"
+                 //type="int"
+                 className="form-control"
+                 name="contact"
+                 placeholder="Enter your hotel contact number"
+                 value={hotelcontact}
+                 onChange={e => onChange(e)}
+                 required
+                 />   
+                </div>
+
+                <div className='form-group'>
+                    <label>City of hotel</label>
                     <input
-                    type="hoteladd"
+                    type="hotelcity"
                       //type="text"
                     className="form-control"
-                    name="hoteladd"
-                    placeholder="Enter your address of hotel"
+                    name="hotelcity"
+                    placeholder="Enter city of your hotel"
+                    value={address}
+                    onChange={e => onChange(e)}
                     required
                     />
                     </div>
