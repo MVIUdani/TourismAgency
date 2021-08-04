9e54395fa65b5f683f99fb1ff913a53fa286d58f
MVIUdani <imeudani@gmail.com>
2021-08-05 01:59:19 +0700

    modify role page

--- frontend/src/components/js/user_role.js
+++ frontend/src/components/js/user_role.js
@@ -1,10 +1,20 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
+import Axios from "axios";
 
 import Customer from './customer/customer_profile';
 import Transportowner from './transport_owner_profile.component';
 
 export default function RolePage(){
     const [role, setRole] = useState("");
+
+    Axios.defaults.withCredentials = true;
+    useEffect(() => {
+      Axios.get("http://localhost:5000/sign-in").then((response) => {
+        if (response.data.loggedIn == true) {
+          setRole(response.data.user[0].role_id);
+        }
+      });
+    }, []);
 
     return(
 <div>
