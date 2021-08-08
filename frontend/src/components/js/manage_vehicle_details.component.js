c796b39e2a8787af59421ad81bc972f567c97f7a
Imesha9725 <imesharishani9725@gmail.com>
2021-08-08 21:13:06 +0530

    Update manage transport ui

--- frontend/src/components/js/manage_vehicle_details.component.js
+++ frontend/src/components/js/manage_vehicle_details.component.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import styles from '../css/transport-owner-details.css';
 
-export default class Vehicle extends Component {
-    render() {
+function Vehicle () {
+    
         return (
 
             
@@ -44,39 +44,8 @@ export default class Vehicle extends Component {
 
 
 
-/*  
-          <div className="auth-wrapper">
-        <div className="auth-inner">
-            <form>
-                <h3>Please fill your vehicles details</h3>
-
-                <div className="form-group">
-                    <label>Vehicle Type</label>
-                    <input type="text" className="form-control" placeholder="vehicle type" />
-                </div>
-
-                <div className="form-group">
-                    <label>Vehicle color</label>
-                    <input type="text" className="form-control" placeholder="Vehicle color" />
-                </div>
-
-                <div className="form-group">
-                    <label>Number of seats</label>
-                    <input type="int" className="form-control" placeholder="Number of seats" />
-                </div>
-                <hr></hr>
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-               
-                
-            </form>
-            </div>
-            </div> 
-             css file is vehicle.css
-            
-            
-            */
-
               
 );
 } 
-}
+
+export default Vehicle;
