e7eef3fb87fd9942279ef29607d3e213155e09fa
MVIUdani <imeudani@gmail.com>
2021-08-24 21:07:59 +0700

    add extra columns in feedback form

--- frontend/src/components/js/home/footer static pages/feedback.js
+++ frontend/src/components/js/home/footer static pages/feedback.js
@@ -9,6 +9,25 @@ export default class Feedback extends Component{
         <div className="feedback-inner">
           <form>
             <h2>Give Your Feedback</h2> 
+           
+            <div className="form-group">
+             <label>First Name</label>
+             <input type="textfeedback"
+                    //type="text" 
+                    className="form-control" 
+                    placeholder="Enter your first name" />
+            </div>
+            <br/>
+
+            <div className="form-group">
+            <label>Last Name</label>
+            <input type="textfeedback"
+                    //type="text" 
+                    className="form-control" 
+                    placeholder="Enter your second name" />
+            </div>
+             <br/>
+
             <div className="form-group">
                     <label>Your Feedback</label>
                     <input type="textfeedback"
@@ -17,11 +36,13 @@ export default class Feedback extends Component{
                     placeholder="Summary" />
                 </div>
                 <br/>
+                
                 <div className="form-group">
                     <label>Futher Details</label>
                     <textarea className="form-control" placeholder="Optionally add more details here..." />
                 </div>
                 <br/>
+
             <button type="submit" className="btn btn-primary btn-block">Submit</button>
 
           </form>
