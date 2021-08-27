25f2b6b0b2d56c0133461eaebc06bf4c23202dc6
MVIUdani <imeudani@gmail.com>
2021-08-27 18:37:26 +0700

    connect request view with backend

--- frontend/src/components/js/request.component.js
+++ frontend/src/components/js/request.component.js
@@ -1,9 +1,32 @@
-import React, { Component } from "react";
+import React, { useState,useEffect } from "react";
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../css/request.css';
+import Axios from "axios";
 
-export default class Requests extends Component{
-    render(){
+export default function Requests (){
+
+    const [requested_places, setRequested_places] = useState("");
+    const [details, setDetails] = useState("");
+    const [user_id, setUser_id] = useState("");
+
+    const addRequest=() => {
+        Axios.post("http://localhost:5000/createRequest",{
+            requested_places: requested_places,
+            details: details,
+        }).then((response) => {
+            console.log(response);
+          });
+    };
+
+    Axios.defaults.withCredentials = true;
+    useEffect(() => {
+      Axios.get("http://localhost:5000/sign-in").then((response) => {
+        if (response.data.loggedIn == true) {
+          setUser_id(response.data.user[0].user_id);
+        }
+    });
+  }, []);
+    
         return(
          <div className="request-wrapper">
          <div className="request-inner">
@@ -11,19 +34,29 @@ export default class Requests extends Component{
              <h2>Request Changes Regarding Visted Places</h2> 
              <div className="form-group">
                      <label>Your Request</label>
-                     <textarea className="form-control" placeholder="request your changes" />
+                     <textarea className="form-control" 
+                     placeholder="request your changes" 
+                     onChange={(e) => {
+                        setRequested_places(e.target.value);
+                      }}
+                     />
                  </div>
                  <br/>
                  <div className="form-group">
                      <label>Futher Details</label>
-                     <textarea className="form-control" placeholder="Optionally add more details here..." />
+                     <textarea className="form-control" 
+                     placeholder="Optionally add more details here..." 
+                     onChange={(e) => {
+                        setDetails(e.target.value);  
+                     }}
+                     />
                  </div>
                  <br/>
-             <button type="submit" className="btn btn-primary btn-block">Submit</button>
+             <button type="submit" className="btn btn-primary btn-block" onClick={addRequest}>Submit</button>
  
            </form>
            </div>
            </div>
         );
-    } 
+ 
  }
