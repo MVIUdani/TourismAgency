b27d25ea677154367407e867a6d736b4a8660a13
Imesha9725 <imesharishani9725@gmail.com>
2021-08-31 21:00:53 +0530

    Add new options into hotel owner

---
+++ frontend/src/components/js/hotelowner/hotel_charge.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import '../../css/hotel_charge.css';
+import { Link } from 'react-router-dom';
+
+function Hotel_charge() {
+
+    return(
+
+        <div>
+            <h1>Hotel chagers update</h1>
+
+        </div>
+
+    );
+    
+}
+
+export default Hotel_charge;
