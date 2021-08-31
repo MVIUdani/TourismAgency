48796b35db3064a85037dd528c8cff5bf4b73178
Imesha9725 <imesharishani9725@gmail.com>
2021-08-31 21:23:31 +0530

    Add form into hotel owner charging cost

--- frontend/src/components/js/hotelowner/hotel_charge.js
+++ frontend/src/components/js/hotelowner/hotel_charge.js
@@ -6,9 +6,38 @@ function Hotel_charge() {
 
     return(
 
-        <div>
+        <div className="Charge_new">
+
             
-            <h1>Hotel chagers update</h1>
+    <form>
+    <div className="Charge">
+    <h1>Hotel chagers update</h1>
+    <p>Please fill your hotel details.</p>
+    <hr></hr>
+
+    <label htmlFor=""><h6>Account owner's name</h6></label>
+    <input type="text" placeholder="Please enter account owner's name" name="branch_name" id="" required></input>
+
+    <label htmlFor=""><h6>Bank Name</h6></label>
+    <input type="text" placeholder="Bank Name" name="bank_name" id="" required></input>
+
+
+    <label htmlFor=""><h6>Branch Name</h6></label>
+    <input type="text" placeholder="Branch Name" name="branch_name" id="" required></input>
+
+    
+    <label htmlFor=""><h6>Account Number</h6></label>
+    <input type="int" placeholder="Please enter your account number" name="branch_name" id="" required></input>
+
+    <input type="hidden" placeholder="" name="user_id" id="" required></input>
+
+
+    <hr></hr>
+
+    <button type="submit" className="registerbtn">Submit</button>
+  </div>
+
+</form>
 
         </div>
 
