b7c3de056aaf5153a7e24c582871004c091b40bd
Imesha9725 <imesharishani9725@gmail.com>
2021-09-03 22:01:49 +0530

    Add vehicle details insert method

---
+++ backend/routes/transport_owner_all_cruds.js
@@ -0,0 +1,25 @@
+const con = require("../db");
+
+module.exports = function(app){
+
+app.post("/api/insert",(req, res)=>{
+
+    const name = req.body.name;
+    const seats = req.body.seats;
+    const color = req.body.color;
+    const valid_date = req.body.valid_date;
+    const cost = req.body.cost;
+    const plate_no = req.body.plate_no;
+    const other_details = req.body.other_details;
+    const user_id = req.body.user_id;
+
+    const sqlInsert = "INSERT INTO vehicle_info (name,seats,color,valid_date,cost,plate_no,other_details,user_id) VALUES (?,?,?,?,?,?,?,3)";
+    db.query(sqlInsert, [name,seats,color,valid_date,cost,plate_no,other_details,user_id] , (err,result) => {
+    console.log(result);
+    res.send(result);
+
+});
+
+});
+
+}
