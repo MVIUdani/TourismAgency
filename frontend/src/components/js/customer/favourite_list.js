672a7e82c94a341e2b29ffe29ad8963fb9d57e22
MVIUdani <imeudani@gmail.com>
2021-09-05 20:01:14 +0700

    add dummy data for faourite page

--- frontend/src/components/js/customer/favourite_list.js
+++ frontend/src/components/js/customer/favourite_list.js
@@ -33,7 +33,29 @@ The capital and the royal palace were abandoned after the king's death. It was u
         
     </div>
 
+    <h2 style={{marginLeft:"45px"}}>Polonnaruwa</h2>
+    <br></br>
+    <div className="Sigiriya"> 
+    <img className="Picture" src='polonnaruwa.jpg' alt="" width="260" height="220"></img>
+    <div>
+    <IconButton aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
         </div>
+
+      <div className="Sigiriya-des">
+      Poḷonnaruwa (Sinhala: පොළොන්නරුව, romanized: Poḷonnaruva; Tamil: பொலன்னறுவ, romanized: Polaṉṉaṟuvai) is the main town of Polonnaruwa District in North Central Province, Sri Lanka. Kaduruwela area is the Polonnaruwa New Town and the other part of Polonnaruwa remains as the royal ancient city of the Kingdom of Polonnaruwa.
+
+The second most ancient of Sri Lanka's kingdoms, Polonnaruwa was first established by the Chola dynasty after their successful invasion of the country's then capital, Anuradhapura, in the 10th century. The Ancient City of Polonnaruwa has been declared a World Heritage Site.[1]
+
+Currently the new Polonnaruwa is undergoing a major development project known as the "Awakening of Polonnaruwa" under the concept of former President Maithripala Sirisena. It envisions the development of all sectors in Polonnaruwa including roads, electricity, agriculture, education, health and environment will be developed comprehensively.
+
+    </div>
+</div>
+
+        </div>
+
+        
 
     );
 }
