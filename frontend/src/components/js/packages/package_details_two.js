e015252a31b7e1bc166e1e9f2d3bc31cb04a4a98
Imesha9725 <imesharishani9725@gmail.com>
2021-09-11 15:47:46 +0530

    Update app.js

---
+++ frontend/src/components/js/packages/package_details_two.js
@@ -0,0 +1,18 @@
+import React, {useState,useEffect} from "react";
+//import '../css/otherhotel.css';
+import { Link } from 'react-router-dom';
+
+function Package_details_two() {
+    return(
+
+            <div className="Aa">
+
+
+
+            </div>
+
+    );
+
+}
+
+export default Package_details_two;
