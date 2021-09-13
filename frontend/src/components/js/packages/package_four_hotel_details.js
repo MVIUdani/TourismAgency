a7112409936200c81ce4b43c65b732d628ad66b4
Imesha9725 <imesharishani9725@gmail.com>
2021-09-13 10:31:57 +0530

    Completed package 04 uis

--- frontend/src/components/js/packages/package_four_hotel_details.js
+++ frontend/src/components/js/packages/package_four_hotel_details.js
@@ -25,7 +25,7 @@ function Package_four_hotel_details(){
 
             <img className="Hotel_size" src='Package4/h1.jpg' alt="" width="470" height="235"></img>
             <img className="Hotel_size" src='Package4/h5.jpg' alt="" width="220" height="200"></img>
-            <img className="Hotel_size" src='Package4/h3.jpg' alt="" width="250" height="220"></img>
+            <img className="Hotel_size" src='Package4/h3.jpg' alt="" width="250" height="210"></img>
             <img className="Hotel_size" src='Package4/h2.jpg' alt="" width="260" height="205"></img>
             <img className="Hotel_size" src='Package4/h4.jpg' alt="" width="200" height="215"></img>
 
@@ -107,9 +107,17 @@ Shangri-La Cares: Our commitment to your well-being in our care
 <div className="Hotel_details">
   <div className="Day">
     <h1>Day 07 & Day 08</h1>
-    <h4>Hotel Name: Grand Hotel Nuwara Eliya</h4><br></br>
+    <h4>Hotel Name: Trinco Blu by Cinnamon hotel</h4><br></br>
     <h4> Hotel Description:</h4>
-    The Grand Hotel is a four-star boutique hotel[1] in Nuwara Eliya, Sri Lanka, that was built in the style of an Elizabethan-era manor house.[2] The hotel has 154 rooms, including three presidential suites, four junior suites, including a governor's suite that have been maintained to preserve the traditional design.[2] The Grand Hotel has a number of restaurants, bars and a billiards room.
+    Trinco Blu by Cinnamon is a luxurious 4-star beachfront resort located 6.5 km from Trincomalee. It provides spacious accommodation with a freshwater pool, 3 dining options and free Wi-Fi.
+
+Simple yet modern rooms are air-conditioned, and equipped with tea/coffee making facilities, cable TV, and a minibar.
+
+Trinco Blu by Cinnamon has a PADI certified dive centre that provides water sport activites such as snorkelling, fishing and scuba diving.
+
+Captain's Deck serves a range of Asian and continental dishes. The Crab Restaurant offers delicious seafood, featuring crab specialities. Drinks and light snacks can be enjoyed at the Rumhold.
+
+Trinco Blu by Cinnamon is located on the north-eastern coast of Sri Lanka, the fifth largest natural harbour in the world. It is 265 km from Bandaranaike International Airport and 260 km from Colombo City.
     <br></br><br></br>
     <h5>If not available this hotel for relevent booked days. We give the other hotel:<Link to='/other_hotel01'><br></br><h4>Hotel Details</h4></Link></h5>
     
@@ -118,12 +126,12 @@ Shangri-La Cares: Our commitment to your well-being in our care
 
 <div className="Hotel_images">
 
-<img className="Hotel_size" src='hotel/hortan_hotel3.jpg' alt="" width="475" height="245"></img>
-<img className="Hotel_size" src='hotel/hortan_hotel2.jpg' alt="" width="260" height="220"></img>
-<img className="Hotel_size" src='hotel/hortan_bed.jpg' alt="" width="240" height="205"></img>
+<img className="Hotel_size" src='Package4/T1.jpg' alt="" width="475" height="245"></img>
+<img className="Hotel_size" src='Package4/T2.jpg' alt="" width="260" height="220"></img>
+<img className="Hotel_size" src='Package4/T3.jpg' alt="" width="240" height="205"></img>
 
-<img className="Hotel_size" src='hotel/hortan_food.jpg' alt="" width="250" height="210"></img>
-<img className="Hotel_size" src='hotel/hortan_food2.jpg' alt="" width="255" height="215"></img>
+<img className="Hotel_size" src='Package4/T4.jpg' alt="" width="250" height="210"></img>
+<img className="Hotel_size" src='Package4/T5.jpg' alt="" width="255" height="215"></img>
 
 </div>
 
