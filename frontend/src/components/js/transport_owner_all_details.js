6b4af927fc93e2201b1321c3845c59526bfea111
Imesha9725 <imesharishani9725@gmail.com>
2021-09-14 16:06:58 +0530

    Geting all inserted data interact with data base-Transport Owner

--- frontend/src/components/js/transport_owner_all_details.js
+++ frontend/src/components/js/transport_owner_all_details.js
@@ -1,28 +1,4 @@
 import React, { Component,useState, useEffect } from "react";
-//import '../css/transport_owner_all_details.css'
-import { Link } from 'react-router-dom';
-import Axios from 'axios';
-
-function Transport_owner_all_details(){
-
-        return(
-
-
-            <div className="All_details">
-
-
-
-            </div>
-
-        );
-
-}
-
-export default Transport_owner_all_details;
-
-
-/*
-import React, { Component,useState, useEffect } from "react";
 import '../css/transport_owner_all_details.css'
 import { Link } from 'react-router-dom';
 import Axios from 'axios';
@@ -30,6 +6,8 @@ import Axios from 'axios';
 function Transport_owner_all_details(){
 
     const [VehicleList, setVehicleList] = useState([]);
+    const [Account_detailsList, setAccount_detailsList] = useState([]);
+    const [BookedDateList, setBookedDateList] = useState([]);
 
     const ShowDetails = () => {
         Axios.get('http://localhost:3001/api/show').then( (response) => {
@@ -38,16 +16,29 @@ function Transport_owner_all_details(){
         });
     };
 
+    const ShowBankAccountDetails = () => {
+        Axios.get('http://localhost:3001/api/showAccount_details').then( (response) => {
+    
+           setAccount_detailsList(response.data);
+        });
+    };
+
+    const ShowBookedDateDetails = () => {
+        Axios.get('http://localhost:3001/api/booked_date_details').then( (response) => {
+    
+            setBookedDateList(response.data);
+        });
+    };
+
     return(
 
         <div className="Monitor">
 
-            <button onClick={ShowDetails} className= "All_button" type="submit">My Vehicle details</button>
-            <button className="All_button" type="submit">My bank account details</button>
-
-        <div className="All_details">
-
+            <button onClick={ShowDetails} className= "All_button" type="submit">Show My Vehicle Details</button>
+            <button onClick={ShowBankAccountDetails} className="All_button" type="submit">Show My bank account details</button>
            
+            <button onClick={ShowBookedDateDetails} className= "All_button" type="submit">Show My booked date</button>
+        <div className="All_details">
          
 
         {VehicleList.map((val,key) => {
@@ -61,37 +52,37 @@ function Transport_owner_all_details(){
                 <br></br>
                 
 
-                <label><h6>Valid date for licence</h6></label>
-            <input type="text" value={val.name}></input>
+                <label><h6>Vehicle type: {val.name}</h6></label>
+            <input type="text" ></input>
 
 
 
-                <label><h6>Valid date for licence</h6></label>
-            <input type="int" value={val.seats}></input>
+                <label><h6>Number Of seats: {val.seats}</h6></label>
+            <input type="int" ></input>
 
               
-                <label><h6>Vehicle Color</h6></label>
-            <input type="text" value={val.color}></input>
+                <label><h6>Vehicle Color: {val.color}</h6></label>
+            <input type="text" ></input>
 
 
 
-                <label><h6>Valid date for licence</h6></label>
-            <input type="text" value={val.valid_date}></input>
+                <label><h6>Valid date for licence: {val.valid_date}</h6></label>
+            <input type="text" ></input>
 
-                <label><h6>Charging for a 1 km</h6></label>
-            <input type="int" value={val.cost}></input>
+                <label><h6>Charging for a 1 km : {val.cost}</h6></label>
+            <input type="int" ></input>
 
                 
-                <label><h6>plate Number</h6></label>
-            <input type="int" value={val.plate_no}></input>
+                <label><h6>plate Number: {val.plate_no}</h6></label>
+            <input type="int" ></input>
 
         
-            <label><h6>Other details</h6></label>
-            <input type="text" value={val.other_details}></input>
+            <label><h6>Other details: {val.other_details}</h6></label>
+            <input type="text" ></input>
 
 
-                <button className="Use_button">Edit</button>
-                <button className="Use_button">Delete</button>
+                <button className="Use_button">Update</button>
+               
                
                 </form>
             </div>
@@ -104,12 +95,90 @@ function Transport_owner_all_details(){
 
 )};
 
-    <div className="Second1">
+    <div className="Tt">
 
-        Hi I sm Imesha.
+      
+    {Account_detailsList.map((val,key) => {
+
+return (
+        <div className="Bank_data">
+            <br></br>
+            <h1>My Bank Account Details</h1>
+            <hr></hr>
+            <form>
+            <br></br>
+
+            <label><h6>Accounter Name:</h6> <h4>{val.user_name}</h4></label>
+        <input type="text" ></input>
+
+            <label><h6>Bank Name :</h6> <h4>{val.bank_name}</h4></label>
+        <input type="int" ></input>
+
+          
+            <label><h6>Branch Name:</h6><h4>{val.branch_name}</h4></label>
+        <input  type="text" ></input>
+
+
+
+            <label><h6>Account Number: </h6> <h4>{val.account_no}</h4></label>
+        <input type="text" ></input>
+
+
+            <button className="Use_button">Update</button>
+           
+           
+            </form>
+        </div>
+       
+
+);
+
+}
+
+
+)};
+
+
+    </div>
+
+
+
+
+    <div className="Tt">
+
+      
+    {BookedDateList.map((val,key) => {
+
+return (
+        <div className="Booked_date">
+            <br></br>
+            <h1>My Booked Date Details</h1>
+            <hr></hr>
+            <form>
+            <br></br>
+
+            <label><h6>Booked Date:</h6> <h4>{val.bookeddate}</h4></label>
+            <input type="date" ></input>
+
+
+
+            <button className="Use_button">Update</button>
+           
+           
+            </form>
+        </div>
+       
+
+);
+
+}
+
+
+)};
+
+
     </div>
  
-
 
         </div>
 
@@ -122,5 +191,3 @@ function Transport_owner_all_details(){
 }
 
 export default Transport_owner_all_details;
-
-*/
