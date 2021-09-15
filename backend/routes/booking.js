3ca0ea600b14f22cad35591a23a0698fc3ae47d0
MVIUdani <imeudani@gmail.com>
2021-09-15 21:06:32 +0700

    add queries in booking

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -9,16 +9,52 @@ module.exports = function(app){
         const price = req.body.price;
 
         con.query(
-            "INSERT INTO booking (no_of_days, no_of_visitors, no_of_rooms, price) VALUES (?,?,?,?)",
+            "INSERT INTO bookings (no_of_days, no_of_visitors, no_of_rooms, price) VALUES (?,?,?,?)",
             [no_of_days, no_of_visitors, no_of_rooms, price],
             (err, result) => {
-                if (err) {
+                if (err) 
+                
+                {
                   console.log(err);
                 } else {
                   res.send("Values Inserted");
                 }
               }
         );
+    });
+
+    app.post("/insertTransport_p2",(req,res)=>{
+    const travel = req.body.travel;
+
+    con.query(
+      "INSERT INTO bookings (transport_type,price,packageID) VALUES (?,17000,2)",
+    [travel],
+    (err,result) => {
+  if(err){
+    console.log(err);
+  }else{
+    res.send("Values Inserted");
+  }
+}
+    )
+    });
+
+    app.post("/insertCount_p2",(req,res)=>{
+      const count = req.body.count;
+      const room_count = req.body.room_count;
+ 
+      con.query(
+        "INSERT INTO bookings (no_of_visitors, no_of_rooms, packageID) VALUES (?,?,'2')",
+        [count,room_count],
+        (err,result) => {
+          if(err){
+            console.log(err);
+          }else{
+            res.send("Values Inserted");
+          }
+        }
+      )
+
     });
 
 }
