b3e368f673218e58bdd475b9d513a11c51e32aba
MVIUdani <imeudani@gmail.com>
2021-09-18 11:58:43 +0700

    add validation in conut passenger views

--- frontend/src/components/js/packages/count/count_five_package.js
+++ frontend/src/components/js/packages/count/count_five_package.js
@@ -1,9 +1,74 @@
 import React,{ useState,useEffect } from 'react';
 import '../../../css/count_pasenger.css';
 import { Link,useHistory } from 'react-router-dom';
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import Count_Validation from './count_validation';
 import Axios from "axios";
 
 function Count_pasenger_five(){
+
+    const history = useHistory();
+
+    const onSubmit=(data)=>{
+        Axios.post("http://localhost:5000/insertCount_p5",data,{
+            count: data.count, //10
+            room_count: data.room_count, //3
+        }).then((response) => {
+            console.log(response);
+          });
+          history.push('/payment-first-Pfive');
+    };
+    
+    return(
+        <div className="Count">
+<Formik
+initialValues={{
+    count:'',
+    room_count:''
+}}
+onSubmit={onSubmit}
+validationSchema={Count_Validation}
+>
+            
+
+            <Form>
+    <div className="First">
+    
+
+    <label htmlFor=""><h5>Number of passengers hope to travel:</h5></label>
+    <Field style={{width:'90%',padding:'12px',margin:' 5px 0 22px 0',display:'inline-block',border:' none',background:'#66606069',marginLeft:'0.2cm'}}
+    //type="int" 
+    placeholder="Enter count of passengers" 
+    name="count" 
+    id="count" 
+    />
+    <ErrorMessage name="count" component="div" style={{color:'red',marginLeft:'0.2cm'}} />
+    
+    <label><h5>Number of rooms that you want:</h5></label>
+    <Field style={{width:'90%',padding:'12px',margin:' 5px 0 22px 0',display:'inline-block',border:' none',background:'#66606069',marginLeft:'0.2cm'}}
+    //type="int" 
+    placeholder="Enter count of rooms" 
+    name="room_count" 
+    id="room_count" 
+    />
+    <ErrorMessage name="room_count" component="div" style={{color:'red',marginLeft:'0.2cm'}} />
+    <br></br>
+        
+        <button className="Count_btn" type="submit">Submit</button>
+        </div>
+        
+
+            </Form>
+           </Formik>
+        </div>
+
+
+    );
+}
+
+export default Count_pasenger_five;
+
+/*function Count_pasenger_five(){
 
     const [count, setCount] = useState("");
     const [room_count, setRoom_count] = useState("");
@@ -67,4 +132,4 @@ function Count_pasenger_five(){
 
 }
 
-export default Count_pasenger_five;
+export default Count_pasenger_five;*/
