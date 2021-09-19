6cf5c0dc623f98196aac11b8488d52cde36523ef
Imesha9725 <imesharishani9725@gmail.com>
2021-09-19 22:28:03 +0530

    Add new js file to transport owner

---
+++ frontend/src/components/js/delete_page.js
@@ -0,0 +1,20 @@
+import React, { Component,useState } from "react";
+//import '../css/hotel_room.css';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
+
+function Delete_page() {
+
+    return(
+
+        <div>
+
+
+            
+        </div>
+
+    );
+    
+}
+
+export default Delete_page;
